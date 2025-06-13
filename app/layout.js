470fa8f6e15fef508f6366987649b00ecbb1a3df
banderGlitch <nernaykumar98@gmail.com>
2025-06-13 11:14:20 +0530

    changes

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,8 @@
 import '../styles/globals.css'
+import '../styles/main.min.css'
+import '../styles/style.css'
+import '../styles/color.css'
+import '../styles/responsive.css'
 import NProgressLoader from './components/NProgressLoader'
 
 export const metadata = {
@@ -11,12 +15,14 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="icon" href="/images/fav.png" type="image/png" sizes="16x16" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/themify-icons@0.1.2/css/themify-icons.css" />
       </head>
       <body>
         <NProgressLoader />
-          <div >
-            {children}
-          </div>
+        <div>
+          {children}
+        </div>
       </body>
     </html>
   )
